19855967d700c524dc8f0eb0dbb4fc40301cac32
Eric Herde <eherde@users.noreply.github.com>
2014-04-18 14:21:13 -0700

    functionalize bootstrap, add kernel install

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,11 +1,48 @@
 #!/bin/bash -e
 
-# the box already comes with sudo and aptitude
 export DEBIAN_FRONTEND=noninteractive
-aptitude update
-aptitude upgrade --assume-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+readonly JOBS=$(($(nproc) * 2))
+_system_update()
+{
+	# the box already comes with sudo and aptitude
+	aptitude update
+	aptitude upgrade --assume-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+}
 
-# install new packages
-readonly PKGS=$(cat /vagrant/pkglist | grep -v "^#" | awk '{print $1}')
-aptitude --assume-yes install ${PKGS}
-aptitude --assume-yes install -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" mysql-server
+_install_packages()
+{
+	local pkgs=$(cat /vagrant/pkglist | grep -v "^#" | awk '{print $1}')
+	aptitude install --assume-yes ${pkgs}
+	aptitude install --assume-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" mysql-server
+}
+
+_install_git()
+{
+	local gitver=1.9.2
+	local gitdir=/root/git.git
+	apt-get build-dep --assume-yes git
+	git clone https://github.com/git/git ${gitdir}
+	cd ${gitdir} && git checkout v${gitver}
+	make prefix=/usr/local install --directory ${gitdir} --jobs ${JOBS}
+}
+
+_install_kernel()
+{
+	local kver=3.12
+	local kdir=/root/linux-${kver}
+	apt-get build-dep --assume-yes linux
+	aptitude install --assume-yes kernel-package
+	cd /root && wget http://www.kernel.org/pub/linux/kernel/v3.0/linux-${kver}.tar.gz && tar --extract --file linux-${kver}.tar.gz
+	cd ${kdir} && cp /vagrant/kernelconfig .config
+	make oldconfig
+	make --jobs ${JOBS}
+	make-kpkg clean
+	fakeroot make-kpkg -j$(($(nproc) * 2)) --initrd kernel_image kernel_headers
+	dpkg --install ../linux-{image,headers}-${kver}.0_${kver}.0-10.00.Custom_amd64.deb
+}
+
+# comment out the actions you want to skip
+#_system_update # required
+#_install_packages # required
+#_install_git # optional
+_install_kernel # optional
