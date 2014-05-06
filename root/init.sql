2d777515e0abfa8a2b9ef5c8e221bdf247c7f146
Eric Herde <eherde@users.noreply.github.com>
2014-05-05 19:07:48 -0700

    initialize database during install
    * create Users and Books tables
    * seed Books table

---
+++ root/init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Users(GUID, Username, Password, SessionID);
+CREATE TABLE Books(Name VARCHAR(255), Price INTEGER);
+INSERT INTO Books VALUES ("Secure Electronic Commerce", 27.50);
+INSERT INTO Books VALUES ("Security Engineering", 45.99);
+INSERT INTO Books VALUES ("Web Security, Privacy, and Commerce", 22.99);
+INSERT INTO Books VALUES ("PHP and MySQL Web Development", 24.99);
+INSERT INTO Books VALUES ("Learning PHP, MySQL, JavaScript, and CSS: A Step-by-Step Guide to Creating Dynamic Websites", 17.27);
+INSERT INTO Books VALUES ("Java Servlet Programming", 39.99);
