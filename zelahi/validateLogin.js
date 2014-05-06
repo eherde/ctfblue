f855aa627da242e39e31b8328747bd9a69f5dd46
zelahi <zelahi@users.noreply.github.com>
2014-05-05 19:15:15 -0700

    create initial HTML5 login page

---
+++ zelahi/validateLogin.js
@@ -0,0 +1,9 @@
+function validateForm() {
+	var x=documents.forms["login"]["username"].value;
+	
+	//ensure that a password and email have been entered
+	if (x == null || x="") {
+		alert("Please enter the required information");
+		return false
+	}
+}
