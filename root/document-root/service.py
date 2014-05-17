8026df0e7f0d6a58a67f0c7c047bbe46f8cf7207
Eric Herde <eherde@users.noreply.github.com>
2014-05-16 17:00:00 -0700

    catch up on documentation for the service

--- root/document-root/service.py
+++ root/document-root/service.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+## @package service
+# The web service
 
 # system modules
 import hashlib
@@ -8,9 +10,10 @@ import time
 import uuid
 import web
 
-sys.dont_write_byte_code = True
+## The directory where the project is run from
 rootdir = os.path.dirname(os.path.abspath(sys.argv[0]))
 
+## Make sure that the service can find its libraries
 sys.path.append(os.path.join(rootdir, 'lib'))
 
 # local modules
@@ -19,18 +22,28 @@ import db
 import scp
 from log import l, exceptions
 
+## Set the path to our configuration
 configfile = os.path.join('ctf-data/ctf.yaml')
+# Set a hook to log uncaught exceptions
 sys.excepthook = exceptions
 
+## Mapping from url => class
 urls = (
 	'/', 'index',
 	'/adduser', 'adduser',
 	'/logon', 'logon'
 )
 
+## Name of authorization cookie
 COOKIE_NAME = 'ctfauth'
+## Cookie expiration time, in seconds
 COOKIE_TTL = 10
 
+##
+# @brief get information specific to this session.
+# This information should not change over the duration of a session.
+#
+# @return dictionary of http information that we care about
 def get_session_info():
 	keys = [ 'HTTP_USER_AGENT', 'HTTP_X_FORWARDED_FOR' ]
 	values = {}
@@ -39,6 +52,10 @@ def get_session_info():
 		values[key] = value
 	return values
 
+##
+# @brief Hash the all values of session information into a single string
+#
+# @return 40 character string that is the hash of HTTP session information
 def get_session_hash():
 	values = get_session_info()
 	h = hashlib.sha1()
@@ -46,13 +63,24 @@ def get_session_hash():
 		h.update(value)
 	return h.hexdigest()
 
+##
+# @brief redirect the user to the logon page.
+#
+# @return returns the logon page
 def logon_redirect():
-	expire_cookie()
 	return web.seeother('/logon')
 
+##
+# @brief Expire the authentication cookie. This is done by setting the timeout to -1
 def expire_cookie():
 	web.setcookie(COOKIE_NAME, '', -1)
 
+##
+# @brief Create the global authentication cookie using the Secure Cookie Protocol
+#
+# @param guid The guid representing the user
+# @param data Any data we wish to store securely with the user.
+# @param session Information that should not change within a session.
 def create_cookie(guid, data, session):
 	# this may produce a slight variation in expiration dates between what we set
 	# and what web.py sets, but we really don't care.
@@ -60,17 +88,31 @@ def create_cookie(guid, data, session):
 	cookie = scp.SecureCookie(guid, expiration, data, get_session_hash())
 	web.setcookie(COOKIE_NAME, cookie.value, COOKIE_TTL, secure=True, httponly=True)
 
+##
+# @brief Determine whether the user is logged onto the system
+#
+# @return True or False
 def logged_on():
 	cookie_data = web.cookies().get(COOKIE_NAME)
 	if cookie_data is None:
 		return False
 	return scp.is_valid(cookie_data, get_session_hash())
 
+##
+# @brief Get the global csrf token, creating it if it does not exist
+#
+# @return the hex value of the guid
 def csrf_token():
 	if 'csrf_token' not in session:
 		session.csrf_token = uuid.uuid4().hex
 	return session.csrf_token
 
+##
+# @brief decorator for protecting requests for forgery
+#
+# @param f The function to decorate
+#
+# @return  The decorated function
 def csrf_protected(f):
 	def decorated(*args, **kwargs):
 		i = web.input()
@@ -82,6 +124,9 @@ def csrf_protected(f):
 		return f(*args, **kwargs)
 	return decorated
 
+##
+# @brief The rendering engine, updated with the directory we care about
+# and the csrf token. This allows templates to reference the csrf token.
 render = web.template.render('templates/', globals={'csrf_token':csrf_token})
 
 ##
@@ -90,14 +135,20 @@ class index:
 	##
 	# @brief generate index
 	#
-	# @return index.html
+	# @return the index page.
 	def GET(self):
 		l.info('GET index')
 		if not logged_on():
 			return logon_redirect()
 		return render.index(None)
 
+##
+# @brief Interface for creating users
 class adduser:
+	##
+	# @brief creates a new user with the provided credentials
+	#
+	# @return the index page
 	def POST(self):
 		l.info('POST adduser')
 		i = web.input()
@@ -121,11 +172,22 @@ class adduser:
 			return render.error(web.ctx.fullpath, 'EXISTS', 'username exists')
 		return render.index(None)
 
+##
+# @brief Interface for logging onto the service
 class logon:
+	##
+	# @brief ensures that the user is not logged on and presents the user
+	# with the logon page.
+	#
+	# @return the logon page
 	def GET(self):
 		l.info('GET logon')
 		expire_cookie()
 		return render.logon()
+	##
+	# @brief attempts to logon to receive an authentication cookie
+	#
+	# @return the index page on success, logon page on failure
 	@csrf_protected
 	def POST(self):
 		l.info('POST logon')
