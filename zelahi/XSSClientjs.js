8b6ea43f76b16213b828ed2d49a6e99d3506faef
zelahi <elahi.zuhayr@gmail.com>
2014-05-23 22:04:45 -0700

    Added XSS in javascript

--- zelahi/XSSClientjs.js
+++ zelahi/XSSClientjs.js
@@ -1,6 +1,7 @@
 function escapeHTML(form){
 			//alert('hello');
-			var unsafe = form.user.value;
-			var safe = unsafe.replace(/&/g, "&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(/"/g,"quot;").replace(/'/g,"&#039;");
-
-		}
+			var unsafe = document.getElementByTagName("input");
+			for(var i = 0; i < unsafe.length; i++) {
+				var safe[i] = unsafe[i].replace(/&/g, "&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(/"/g,"quot;").replace(/'/g,"&#039;");
+			}
+}
